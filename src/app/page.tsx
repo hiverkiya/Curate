3a68bc976d1bbd2799453f60ba02531f505d62ed
hiverkiya <hiverkiya@gmail.com>
2026-01-06 12:39:22 +0530

    Setting up NextJs 16, Shadcn and Tailwind CSS. Also modified the globals.css for the default dark theme

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div>
+      Hello
+      <Button variant="default">Click me</Button>
+    </div>
+  );
+}
