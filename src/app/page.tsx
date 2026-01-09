a5cc16da44529e005ff5eebeb0747fe11daacb03
hiverkiya <hiverkiya@gmail.com>
2026-01-09 14:43:48 +0530

    Designing Home screen projects layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,6 @@
-"use client";
-import { Button } from "@/components/ui/button";
-import { useMutation, useQuery } from "convex/react";
-import { api } from "../../convex/_generated/api";
+import { ProjectsView } from "@/features/projects/components/projects-view";
 
-export default function Page() {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-  return (
-    <div>
-      Add a project <br />
-      <Button
-        onClick={() => createProject({ name: "New Project" })}
-        variant="default"
-      >
-        Add a project
-      </Button>
-      <div>
-        {projects?.map((project) => (
-          <h6 key={project.ownerId}>
-            {project.name} <span>{project.ownerId}</span>
-          </h6>
-        ))}
-      </div>
-    </div>
-  );
-}
+const Home = () => {
+  return <ProjectsView />;
+};
+export default Home;
